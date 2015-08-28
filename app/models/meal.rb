d39883c7a4e056667111e1fd86e93881ee675b9c
imeshcheryakova <imeshcher@chicagobooth.edu>
2015-08-27 22:26:26 -0500

    heroku fix 3

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,7 +1,7 @@
 class Meal < ActiveRecord::Base
 
   validates :meal, :presence => true
-  validates :calories_per_portion, :presence => true, numericality: { only_integer: true,  greater_than: 0, less_than: 4000 }
+  validates :calories_per_portion, :presence => true, numericality: { only_integer: true,  greater_than_or_equal_to: 0, less_than: 4000 }
   #validates :serving_size, :presence => true
   validates :content, :presence => true
   has_many :recommendations
